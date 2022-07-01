33837182b171fa28cc7e16fc0ca36f7b6a921a2c
oneway-fullstack <trust.biz888@gmail.com>
2022-07-01 23:32:41 +0800

    unit test modify

--- src/components/ReactTalk/index.test.tsx
+++ src/components/ReactTalk/index.test.tsx
@@ -1,9 +1,17 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import ReactTalk from './index';
+import { act } from 'react-dom/test-utils';
 
-test('renders ReactTalk Button', () => {
+test('ReactTalk Button', () => {
   render(<ReactTalk />);
+ 
   const btnElement = screen.getByText(/React/i);
   expect(btnElement).toBeInTheDocument();
+
+  act(() => {
+    window.dispatchEvent(new CustomEvent('MootoolsTalk', {detail: 'Three'}));
+  });
+
+  expect(btnElement.innerHTML).toBe('Three');
 });
